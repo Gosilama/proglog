7960b883f6f65520be01d3a0dbe26ddab23d66e8
gosilama <osilamagodspower@gmail.com>
2023-01-17 23:00:57 -0600

    chore: added the append method to segment

--- internal/log/segment.go
+++ internal/log/segment.go
@@ -2,6 +2,8 @@ package log
 
 import (
 	"fmt"
+	api "github.com/gosilama/proglog/api/v1"
+	"google.golang.org/protobuf/proto"
 	"os"
 	"path"
 )
@@ -46,4 +48,23 @@ func newSegment(dir string, baseOffset uint64, c Config) (*segment, error) {
 	}
 
 	return s, nil
+}
+
+func (s *segment) Append(record *api.Record) (offset uint64, err error) {
+	cur := s.nextOffset
+	record.Offset = cur
+	p, err := proto.Marshal(record)
+	if err != nil {
+		return 0, err
+	}
+
+	_, pos, err := s.store.Append(p)
+	if err != nil {
+		return 0, err
+	}
+	if err = s.index.Write(uint32(s.nextOffset-uint64(s.baseOffset)), pos); err != nil {
+		return 0, err
+	}
+	s.nextOffset++
+	return cur, nil
 }
